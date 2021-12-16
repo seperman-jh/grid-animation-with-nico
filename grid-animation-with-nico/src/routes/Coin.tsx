475636b2d3ad79a4e9178b557cc22ab55926acd7
seperman.jh <seperman.jh@gmail.com>
2021-12-15 22:16:42 -0800

    #first commit

--- grid-animation-with-nico/src/routes/Coin.tsx
+++
@@ -1,7 +0,0 @@
-import {useLocation} from "react-router-dom";
-
-function Coin() {
-    const {state}  = useLocation() // 6.0 부터 typescript generic 지원 안함. 기존 Interface 상속 받아옴.
-    return <h1>{state?.name}</h1>; // 기본값 NULL 처리. optional chaining. https://ko.javascript.info/optional-chaining
-}
-export default Coin;
